1c4f132131108d4a7811365a8f85b61bc18c23bb
[firstname lastname] <helloworldprogrammer612@gmail.com>
2021-03-31 13:47:14 +0530

    third commit

--- plan/settings.py
+++ plan/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-CORS_ALLOWED_ORIGINS = [hr
+CORS_ALLOWED_ORIGINS = [
     'https://today-plans.herokuapp.com',
     'http://today-plans.herokuapp.com',
     
